e311e675e8312b111d03357f95fdf1e868894a07
Adrian Doonan <adriandoonan@gmail.com>
2025-09-25 14:05:54 +0200

    cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
-
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
+  adapter: cloudflare(),
   server: { host: true },
   markdown: { gfm: true }
 });
